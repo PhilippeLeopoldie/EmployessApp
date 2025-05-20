01e800a598521f4f0d10c53f129b11b2320f4548
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-05-20 12:06:25 +0200

    Add IndexVm and IndexVmItems classes with validation
    Introduces the `IndexVm` class in the `EmployeesApp.Web.Views.Employees` namespace, which includes a nested `IndexVmItems` class. The `IndexVmItems` class defines properties such as `Id`, `Name`, `Email`, and `showAsHighlighted`, with validation attributes for `Name` and `Email`. The `IndexVm` class also contains a property for an array of `IndexVm` objects, ensuring required data integrity and validation.

---
+++ EmployeesApp.Web/Views/Employees/IndexVm.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EmployeesApp.Web.Models;
+namespace EmployeesApp.Web.Views.Employees;
+
+public class IndexVm
+{
+    public class IndexVmItems
+    {
+        public required int Id { get; set; }
+
+        [Required(ErrorMessage = "You must specify a name")]
+        [SuspiciousPerson("Pontus Wittenmark", ErrorMessage = "Suspicious person detected!")]
+        public required string Name { get; set; }
+
+        [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "Invalid e-mail address")]
+        [Required(ErrorMessage = "You must specify an e-mail address")]
+        public required string Email { get; set; }
+
+        public required bool showAsHighlighted { get; set; }
+    }
+
+
+    public required IndexVm[] ListOfIndexVmItems { get; set; }
+}
